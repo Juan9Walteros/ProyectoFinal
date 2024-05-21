c3eeddcc457ebeb09d4b95aa692a7efa8bae3bb9
juan9walteros <80785635+Juan9Walteros@users.noreply.github.com>
2024-05-21 09:32:16 -0500

    modificaciones al css

--- src/components/About.js
+++ src/components/About.js
@@ -1,59 +1,88 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import imagen from '../img/Vector_1.png'; 
-import imagen1 from '../img/Vector_2.png';    
-import imagen2 from '../img/Group.png';    
-import imagen4 from '../img/Element_1.png';     
-import React from "react";  
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import imagen from "../img/Vector_1.png";
+import imagen1 from "../img/Vector_2.png";
+import imagen2 from "../img/Group.png";
+import imagen4 from "../img/Element_1.png";
+import React from "react";
 
 function About() {
   return (
     <>
-    <div className='About'>
-    <Container>
-      <Row>
-        <Col xs={12} md={12}>
-            <div className='thing align-items-center'>
-            <h4 className='thing1 mb-0'>Things you need <span>to do</span> </h4>
-            </div>
-            <div className='thing2'>
-            <p>We ensure that you'll embark on a perfectly planned, <br/>safe vacation at a price you can afford.</p>
-            
-            </div>
-            <img src={imagen4} alt="Imagen 1" className="img1 img-fluid " />
-        </Col>
+      <div className="About">
+        <Container>
+          <Row>
+            <Col xs={12} md={12}>
+              <div className="thing align-items-center">
+                <h4 className="thing1 mb-0">
+                  Things you need <span>to do</span>{" "}
+                </h4>
+              </div>
+              <div className="thing2">
+                <p>
+                  We ensure that you'll embark on a perfectly planned, <br />
+                  safe vacation at a price you can afford.
+                </p>
+              </div>
+              <img src={imagen4} alt="Imagen 1" className="img1 img-fluid " />
+            </Col>
+          </Row>
+          <div className="d-flex">
+            <div className="cont1 container">
+              <div className="row">
+                <div className="col1 col">
+                  <img
+                    src={imagen}
+                    alt="Imagen 1"
+                    className="img-fluid d-flex pb-5"
+                    style={{ marginLeft: "10px", marginTop: "30px" }}
+                  />
+                  <h4 className="pb-1 d-flex" style={{ marginLeft: "10px" }}>
+                    Sign Up
+                  </h4>
+                  <p className="flex-wrap" style={{ marginLeft: "10px" }}>
+                    Completes all the work associated with planning and
+                    processing
+                  </p>
+                </div>
 
-      </Row>
-      <div className='d-flex'> 
-            <div className="cont1 container"> 
-            <div className="row"> 
-            <div className="col1 col"> 
-            
-            <img src={imagen} alt="Imagen 1" className="img-fluid d-flex pb-5" style={{marginLeft:'10px',marginTop:'30px'}}/>
-            <h4 className='pb-1 d-flex' style={{marginLeft:'10px'}}>Sign Up</h4>
-            <p className="flex-wrap" style={{marginLeft:'10px'}}>Completes all the work associated with planning and processing</p>
+                <div className="col1 col">
+                  <img
+                    src={imagen1}
+                    alt="Imagen 1"
+                    className="img-fluid d-flex pb-5"
+                    style={{ marginLeft: "10px", marginTop: "30px" }}
+                  />
+                  <h4 className="pb-1 d-flex" style={{ marginLeft: "10px" }}>
+                    Worth of Money
+                  </h4>
+                  <p className="text-flex" style={{ marginLeft: "10px" }}>
+                    After successful access the book from axclusive deals &
+                    pricing
+                  </p>
+                </div>
+                <div className="col1 col">
+                  <img
+                    src={imagen2}
+                    alt="Imagen 1"
+                    className="img-fluid d-flex pb-5"
+                    style={{ marginLeft: "10px", marginTop: "30px" }}
+                  />
+                  <h4 className="pb-1 d-flex" style={{ marginLeft: "10px" }}>
+                    Exciting Tavel
+                  </h4>
+                  <p className="text-flex" style={{ marginLeft: "10px" }}>
+                    Start and explore a wide range of exciting travel
+                    experience.
+                  </p>
+                </div>
+              </div>
             </div>
-           
-            <div className="col1 col"> 
-            <img src={imagen1} alt="Imagen 1" className="img-fluid d-flex pb-5" style={{marginLeft:'10px',marginTop:'30px'}} />
-          <h4 className='pb-1 d-flex' style={{marginLeft:'10px'}}>Worth of Money</h4>
-          <p className='text-flex' style={{marginLeft:'10px'}}>After successful access the book from axclusive deals & pricing</p>
-            </div> 
-            <div className="col1 col"> 
-            <img src={imagen2} alt="Imagen 1" className="img-fluid d-flex pb-5"style={{marginLeft:'10px',marginTop:'30px'}}/>
-          <h4 className='pb-1 d-flex' style={{marginLeft:'10px'}}>Exciting Tavel</h4>
-          <p className='text-flex' style={{marginLeft:'10px'}}>Start and explore a wide range of exciting travel experience.</p>
-            </div> 
-            </div> 
-            </div> 
-            </div>
-      
-    </Container>
-    </div>
-    
+          </div>
+        </Container>
+      </div>
     </>
-    
   );
 }
 
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,94 +1,104 @@
-import { Card, CardBody,CardText} from "react-bootstrap";
+import { Card, CardBody, CardText } from "react-bootstrap";
 import Slider from "react-slick";
 import React from "react";
-import Img1 from '../img/Image-2.png' 
-import Img2 from '../img/Image-3.png'
-import Img3 from '../img/Image-4.png'
-import Img4 from '../img/Image-5.png'
+import Img1 from "../img/Image-2.png";
+import Img2 from "../img/Image-3.png";
+import Img3 from "../img/Image-4.png";
+import Img4 from "../img/Image-5.png";
 import Footer from "../layouts/footer";
 
-const Blog= () => {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed:3000,
-    };
-    return(
-        <>
-        <div className="principalBlog">
-            <div className="seccion1">
-                <h1 className="getUp">Get Update with <span>latest blog</span></h1>
-            </div> 
-            <div className="seccion2">
-                <div className="cards7">
-                    <Slider {...settings}>
-                        <div>
-                        <Card className="card7 card">
-                            <Card.Img variant="top" src={Img1} />
-                            <CardBody>
-                                <CardText>
-                                    The Amazing Difference a Year Of Travelling.
-                                </CardText>
-                                <p className="text-body-secondary">July 27, 2021</p>
-                            </CardBody>
-                         </Card>
-                        </div>
-                        <div>
-                        <Card className="card7 card">
-                            <Card.Img variant="top" src={Img2} />
-                            <CardBody>
-                                <CardText>
-                                    Travel for enough, you meet yourself.
-                                </CardText>
-                                <p className="text-body-secondary">July 27, 2021</p>
-                            </CardBody>
-                         </Card>
-                        </div>
-                        <div>
-                        <Card className="card7 card">
-                            <Card.Img variant="top" src={Img3} />
-                            <CardBody>
-                                <CardText>
-                                The Amazing Difference a Year Of Travelling.
-                                </CardText>
-                                <p className="text-body-secondary">July 27, 2021</p>
-                            </CardBody>
-                         </Card>
-                        </div>
-                        <div>
-                        <Card className="card7 card">
-                            <Card.Img variant="top" src={Img4} />
-                            <CardBody>
-                                <CardText>
-                                    The Amazing Difference a Year Of Travelling.
-                                </CardText>
-                                <p className="text-body-secondary">July 27, 2021</p>
-                            </CardBody>
-                         </Card>
-                        </div>
-                    </Slider>
-                </div>
-            </div> 
+const Blog = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+  return (
+    <>
+      <div className="principalBlog">
+        <div className="seccion1">
+          <h1 className="getUp">
+            Get Update with <span>latest blog</span>
+          </h1>
         </div>
-        <div className="suscribe container">
-            <div className="PrincipalCon">
-                <div className="txtSuscribe">
-                    <h1 className="titulo1"> Suscribe and get exclusive <br />deals & offer</h1>
-                </div>
-                <form className="email">
-                    <input className="emailIn"type="email" placeholder="Enter your email"></input>
-                    <button className="submit-button" type="submit">Subscribe</button>
-                </form>
-                <div className="Circulo1"></div>
-            </div>
+        <div className="seccion2">
+          <div className="cards7">
+            <Slider {...settings}>
+              <div>
+                <Card className="card7 card">
+                  <Card.Img variant="top" src={Img1} />
+                  <CardBody>
+                    <CardText>
+                      The Amazing Difference a Year Of Travelling.
+                    </CardText>
+                    <p className="text-body-secondary">July 27, 2021</p>
+                  </CardBody>
+                </Card>
+              </div>
+              <div>
+                <Card className="card7 card">
+                  <Card.Img variant="top" src={Img2} />
+                  <CardBody>
+                    <CardText>Travel for enough, you meet yourself.</CardText>
+                    <p className="text-body-secondary">July 27, 2021</p>
+                  </CardBody>
+                </Card>
+              </div>
+              <div>
+                <Card className="card7 card">
+                  <Card.Img variant="top" src={Img3} />
+                  <CardBody>
+                    <CardText>
+                      The Amazing Difference a Year Of Travelling.
+                    </CardText>
+                    <p className="text-body-secondary">July 27, 2021</p>
+                  </CardBody>
+                </Card>
+              </div>
+              <div>
+                <Card className="card7 card">
+                  <Card.Img variant="top" src={Img4} />
+                  <CardBody>
+                    <CardText>
+                      The Amazing Difference a Year Of Travelling.
+                    </CardText>
+                    <p className="text-body-secondary">July 27, 2021</p>
+                  </CardBody>
+                </Card>
+              </div>
+            </Slider>
+          </div>
         </div>
-        <Footer/>
-        </>
-    )
-}
+      </div>
+      <div className="suscribe">
+        <div className="PrincipalCon">
+          <div className="txtSuscribe">
+            <h1 className="titulo1">
+              {" "}
+              Suscribe and get exclusive <br />
+              deals & offer
+            </h1>
+          </div>
+          <form className="email">
+            <input
+              className="emailIn"
+              type="email"
+              placeholder="Enter your email"
+            ></input>
+            <button className="submit-button" type="submit">
+              Subscribe
+            </button>
+          </form>
+          <div className="Circulo1"></div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Blog;
